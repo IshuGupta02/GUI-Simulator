136f79b1e10c2e9d94f7d995947aeea48ad9a39e
Richa-iitr <richa31102@gmail.com>
2023-01-16 04:46:49 +0530

    added packet detail window and disabled packets demo

--- L1CacheStatic.py
+++ L1CacheStatic.py
@@ -1,12 +1,30 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
 
+class PacketWindow(QMainWindow):                           # <===
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("packet_details.ui", self)
+
 class MyGUI(QMainWindow):
 
     def __init__(self):
         super(MyGUI, self).__init__()
         uic.loadUi("qtDesigner.ui", self)
         self.show()
+
+        self.pkt2.setEnabled(False)
+        self.pkt3.setEnabled(False)
+        self.pkt4.setEnabled(False)
+        self.pkt5.setEnabled(False)
+        self.pkt6.setEnabled(False)
+        self.pkt7.setEnabled(False)
+
+        self.pkt1.clicked.connect(self.displayPacket)
+
+    def displayPacket(self):
+        self.w = PacketWindow()
+        self.w.show()
 
 def main():
     app = QApplication([])
