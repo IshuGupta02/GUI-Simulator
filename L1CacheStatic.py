baca1dab57dc6a46dff8ec28ba9988fa8ccd2134
Ishu Gupta <ishugupta0298@gmail.com>
2023-01-16 08:04:46 +0530

    Initial Commit- incomplete gui

---
+++ L1CacheStatic.py
@@ -0,0 +1,10 @@
+from tkinter from *
+
+root = Tk()
+
+input_queues = ["queue1", "queue1", "queue1", "queue1", "queue1"]
+output_queues = ["queue1", "queue1", "queue1", "queue1", "queue1"]
+
+
+
+root.mainloop()
