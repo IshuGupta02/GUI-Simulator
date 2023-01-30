1767b83a7217614fccd158ac80a05bc657f4768b
Ishu Gupta <ishugupta0298@gmail.com>
2023-01-30 10:15:44 +0530

    Add functionality to start, previous, next buttons

--- L1CacheStatic.py
+++ L1CacheStatic.py
@@ -1,7 +1,8 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
+from assets import parse
 
-class PacketWindow(QMainWindow):                           # <===
+class PacketWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("packet_details.ui", self)
@@ -12,19 +13,77 @@ class MyGUI(QMainWindow):
         super(MyGUI, self).__init__()
         uic.loadUi("qtDesigner.ui", self)
         self.show()
+        self.data = parse.parse_data()
+        self.queues = parse.queue_details()
 
-        self.pkt2.setEnabled(False)
-        self.pkt3.setEnabled(False)
-        self.pkt4.setEnabled(False)
-        self.pkt5.setEnabled(False)
-        self.pkt6.setEnabled(False)
-        self.pkt7.setEnabled(False)
+        self.cycle=0
 
-        self.pkt1.clicked.connect(self.displayPacket)
-
+        self.Start.setEnabled(True)
+        self.Start.clicked.connect(self.startDisplay)
+        
     def displayPacket(self):
         self.w = PacketWindow()
         self.w.show()
+
+    def displayCycle(self, cycle):
+        if cycle<0:
+            self.cycle = len(self.data)+cycle
+            cycle = self.cycle
+        elif cycle>= len(self.data):
+            self.cycle = cycle % len(self.data)
+            cycle = self.cycle
+        else:
+            self.cycle = cycle
+
+        self.cycleNumber.display(self.cycle)
+        print("displayCycle :", self.cycle)
+
+        all_queues = self.data[cycle]
+
+        for i, queue in enumerate(all_queues):
+            queue_name = self.queues[i]
+            
+            for idx, pkt in enumerate(queue):
+                buttonName = queue_name+"_"+str(idx+1)
+
+                # remove this try except once ui changes in all queues is done
+                try:
+                    x = getattr(self, buttonName)
+                    x.setText(pkt)
+                    if pkt!="NaN":
+                        x.setEnabled(True)
+                except:
+                    pass
+
+            
+    def enableSupportButtons(self):
+        self.Next.setEnabled(True)
+        self.Next.clicked.connect(lambda : self.nextCycle(1))
+
+        self.Next_5.setEnabled(True)
+        self.Next_5.clicked.connect(lambda : self.nextCycle(5))
+
+        self.Next_10.setEnabled(True)
+        self.Next_10.clicked.connect(lambda : self.nextCycle(10))
+
+        self.Previous.setEnabled(True)
+        self.Previous.clicked.connect(lambda : self.prevCycle(1))
+
+        self.Previous_5.setEnabled(True)
+        self.Previous_5.clicked.connect(lambda : self.prevCycle(5))
+
+        self.Previous_10.setEnabled(True)
+        self.Previous_10.clicked.connect(lambda : self.prevCycle(10))
+
+    def startDisplay(self):
+        self.displayCycle(0)
+        self.enableSupportButtons()
+    
+    def nextCycle(self, inc=1):
+        self.displayCycle(self.cycle+inc)
+
+    def prevCycle(self, dec=1):
+        self.displayCycle(self.cycle-dec)
 
 def main():
     app = QApplication([])
--- assets/parse.py
+++ assets/parse.py
@@ -1,50 +1,56 @@
-f =  open("assets/L1DCache","r") 
+def queue_details():
+    queues = ['RDREQ','WRREQ','PBR_REQ','PBR_RSP','FILL','MISS','VICTIM','PRB_REQ','PBR_RESP','RDRESP']
+    return queues
 
-queues = ['RDREQ','WRREQ','PBR_REQ','PBR_RSP','FILL','MISS','VICTIM','PRB_REQ','PBR_RESP','RDRESP']
-cyc_data = []
-for line in f:
-    line = line.strip()
-    cyc_num_start_index = line.find('Cycle$') + 6
-    cyc_num_end_index = line.find('\t',cyc_num_start_index) - 1
-    cyc_num = int(line[cyc_num_start_index:cyc_num_end_index+1])
+def parse_data():
 
-    curr_cyc_data = []
-    for queue in queues:
-        data_st_ind = line.find(queue) + len(queue)
-        queue_space_st = line.find('M',data_st_ind) + 2
-        queue_space_en = line.find('~',queue_space_st)
-        queue_space = int(line[queue_space_st:queue_space_en])
-        queue_empty_st = line.find('A',queue_space_en) + 2
-        queue_empty_en = line.find('~',queue_empty_st)
-        queue_empty = int(line[queue_empty_st:queue_empty_en])
-        queue_occupied_st = line.find('C',queue_empty_en) + 2
-        queue_occupied_en = line.find(')',queue_occupied_st)
-        queue_occupied = int(line[queue_occupied_st:queue_occupied_en])
+    f =  open("assets/L1DCache","r") 
 
-        queue_data_st = line.find(':',queue_occupied_en)
-        queue_data_en = line.find(';',queue_data_st)
-        i = queue_data_st + 1
-        queue_data = []
-        while True:
-            if line[i]==';':
-                break
-            if line[i]==',':
-                queue_data.append('NaN')
-                i+=1
-            else:
-                data_en = line.find(' ',i)
-                cur_data = int(line[i:data_en])
-                queue_data.append(cur_data)
-                i = data_en + 1
-        
-        #write a check function if entries consistent with the data in queue
-        curr_cyc_data.append(queue_data)
-    cyc_data.append(curr_cyc_data)
+    queues = ['RDREQ','WRREQ','PBR_REQ','PBR_RSP','FILL','MISS','VICTIM','PRB_REQ','PBR_RESP','RDRESP']
+    cyc_data = []
+    print_=True
+    for line in f:
+        line = line.strip()
+        cyc_num_start_index = line.find('Cycle$') + 6
+        cyc_num_end_index = line.find('\t',cyc_num_start_index) - 1
+        cyc_num = int(line[cyc_num_start_index:cyc_num_end_index+1])
 
-print(cyc_data)
+        curr_cyc_data = []
+        for queue in queues:
+            data_st_ind = line.find(queue) + len(queue)
+            queue_space_st = line.find('M',data_st_ind) + 2
+            queue_space_en = line.find('~',queue_space_st)
+            queue_space = int(line[queue_space_st:queue_space_en])
+            queue_empty_st = line.find('A',queue_space_en) + 2
+            queue_empty_en = line.find('~',queue_empty_st)
+            queue_empty = int(line[queue_empty_st:queue_empty_en])
+            queue_occupied_st = line.find('C',queue_empty_en) + 2
+            queue_occupied_en = line.find(')',queue_occupied_st)
+            queue_occupied = int(line[queue_occupied_st:queue_occupied_en])
+
+            queue_data_st = line.find(':',queue_occupied_en)
+            queue_data_en = line.find(';',queue_data_st)
+
+            if print_:
+                print("Queue: ", queue, " queue_space: ", queue_space, " queue_empty: ", queue_empty, " queue_occupied: ", queue_occupied)
+
+            i = queue_data_st + 1
+            queue_data = []
+            while True:
+                if line[i]==';':
+                    break
+                if line[i]==',':
+                    queue_data.append('NaN')
+                    i+=1
+                else:
+                    data_en = line.find(' ',i)
+                    cur_data = int(line[i:data_en])
+                    queue_data.append(cur_data)
+                    i = data_en + 1
             
-
-
-
-
-
+            #write a check function if entries consistent with the data in queue
+            curr_cyc_data.append(queue_data)
+        print_ = False
+        cyc_data.append(curr_cyc_data)
+    
+    return cyc_data
