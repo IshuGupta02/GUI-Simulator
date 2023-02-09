94f1bd504571391316bdfcacce54ba21ed493d2e
Ishu Gupta <ishugupta0298@gmail.com>
2023-02-09 19:17:16 +0530

    Made changes to the queues in UI

---
+++ archives/parse.py
@@ -0,0 +1,62 @@
+def queue_details():
+    queues = ['RDREQ','WRREQ','PBR_REQ','PBR_RSP','FILL','MISS','VICTIM','PRB_REQ','PBR_RESP','RD_RESP']
+    return queues
+
+def parse_data():
+
+    f =  open("assets/files/L1ICache","r") 
+
+    queues = queue_details()
+    cyc_data = []
+    print_=True
+    for line in f:
+        line = line.strip()
+        cyc_num_start_index = line.find('Cycle$') + 6
+        cyc_num_end_index = line.find('\t',cyc_num_start_index)-1
+       
+        cyc_num = int(line[cyc_num_start_index:cyc_num_end_index+1])
+        curr_cyc_data = {}
+        for queue in queues:
+
+            data_st_ind = line.find(queue) + len(queue)
+            queue_space_st = line.find('M',data_st_ind) + 2
+            queue_space_en = line.find('~',queue_space_st)
+            queue_space = int(line[queue_space_st:queue_space_en])
+            queue_empty_st = line.find('A',queue_space_en) + 2
+            queue_empty_en = line.find('~',queue_empty_st)
+            queue_empty = int(line[queue_empty_st:queue_empty_en])
+            queue_occupied_st = line.find('C',queue_empty_en) + 2
+            queue_occupied_en = line.find(')',queue_occupied_st)
+            queue_occupied = int(line[queue_occupied_st:queue_occupied_en])
+
+            queue_data_st = line.find(':',queue_occupied_en)
+            queue_data_en = line.find(';',queue_data_st)
+
+            i = queue_data_st + 1
+            queue_data = []
+            while True:
+                if line[i]==';':
+                    break
+                if line[i]==',':
+                    queue_data.append('NaN')
+                    i+=1
+                else:
+                    data_en_1 = line.find(' ',i)
+                    data_en_2 = line.find(',',i)
+                    data_en = min(data_en_1, data_en_2)
+                    cur_data = int(line[i:data_en])
+                    queue_data.append(cur_data)
+                    i = data_en + 1
+            
+            if print_:
+                print("Queue: ", queue, " queue_space: ", queue_space, " queue_empty: ", queue_empty, " queue_occupied: ", queue_occupied)
+                print("Queue Data: ", queue, ": ", queue_data)
+            #write a check function if entries consistent with the data in queue
+            curr_cyc_data[queue] = queue_data
+
+        print_ = False
+        cyc_data.append(curr_cyc_data)
+
+    return cyc_data
+
+(parse_data())
--- assets/parse.py
+++ assets/parse.py
@@ -1,58 +1,96 @@
-def queue_details():
-    queues = ['RDREQ','WRREQ','PBR_REQ','PBR_RSP','FILL','MISS','VICTIM','PRB_REQ','PBR_RESP','RD_RESP']
-    return queues
+import os, timeit
 
 def parse_data():
 
-    f =  open("assets/L1DCache","r") 
+    f =  open("assets/files/L1ICache","r") 
 
-    queues = queue_details()
     cyc_data = []
     print_=True
+
+    line_num = 0
+
     for line in f:
         line = line.strip()
         cyc_num_start_index = line.find('Cycle$') + 6
-        cyc_num_end_index = line.find('\t',cyc_num_start_index) - 1
-        cyc_num = int(line[cyc_num_start_index:cyc_num_end_index+1])
-
-        curr_cyc_data = []
-        for queue in queues:
-            data_st_ind = line.find(queue) + len(queue)
-            queue_space_st = line.find('M',data_st_ind) + 2
-            queue_space_en = line.find('~',queue_space_st)
-            queue_space = int(line[queue_space_st:queue_space_en])
-            queue_empty_st = line.find('A',queue_space_en) + 2
-            queue_empty_en = line.find('~',queue_empty_st)
-            queue_empty = int(line[queue_empty_st:queue_empty_en])
-            queue_occupied_st = line.find('C',queue_empty_en) + 2
-            queue_occupied_en = line.find(')',queue_occupied_st)
-            queue_occupied = int(line[queue_occupied_st:queue_occupied_en])
-
-            queue_data_st = line.find(':',queue_occupied_en)
-            queue_data_en = line.find(';',queue_data_st)
-
-            if print_:
-                print("Queue: ", queue, " queue_space: ", queue_space, " queue_empty: ", queue_empty, " queue_occupied: ", queue_occupied)
-
-            i = queue_data_st + 1
-            queue_data = []
-            while True:
-                if line[i]==';':
-                    break
-                if line[i]==',':
-                    queue_data.append('NaN')
-                    i+=1
-                else:
-                    data_en_1 = line.find(' ',i)
-                    data_en_2 = line.find(',',i)
-                    data_en = min(data_en_1, data_en_2)
-                    cur_data = int(line[i:data_en])
-                    queue_data.append(cur_data)
-                    i = data_en + 1
-            
-            #write a check function if entries consistent with the data in queue
-            curr_cyc_data.append(queue_data)
-        print_ = False
-        cyc_data.append(curr_cyc_data)
+        cyc_num_end_index = line.find('\t',cyc_num_start_index)
     
+        cyc_num = int(line[cyc_num_start_index:cyc_num_end_index])
+
+        print("Loading data for cycle number : ",cyc_num, " ,Line number: ", line_num)
+        if cyc_num != line_num:
+            print("Cycle Number is not equal to Line number at ", line_num)
+            break
+
+        line_num +=1
+
+        curr_cyc_data = {}
+        
+        line = line[cyc_num_end_index:].strip()
+
+        while len(line)>0:
+            curr_dollar = line.find('$')
+            token = line[:curr_dollar].strip()
+            next_dollar = line.find('$', curr_dollar+1)
+            if next_dollar!=-1:
+                last_space = line[:next_dollar].rfind('\t')
+                curr_line = line[curr_dollar+1:last_space].strip()
+                line = line[last_space:].strip()
+            else:
+                curr_line = line[curr_dollar+1:].strip()
+                line = ""
+
+            all_queues = {}
+            
+            while len(curr_line)>0:
+                queue_data = {}
+                name_end = curr_line.find('(')
+                name = curr_line[:name_end]
+                curr_line = curr_line[name_end:]
+
+                m = int(curr_line[curr_line.find('M')+2:curr_line.find('~')].strip())
+                curr_line = curr_line[curr_line.find('~')+1:]
+
+                a = int(curr_line[curr_line.find('A')+2:curr_line.find('~')].strip())
+                curr_line = curr_line[curr_line.find('~')+1:]
+
+                c = int(curr_line[curr_line.find('C')+2:curr_line.find(')')].strip())
+                curr_line = curr_line[curr_line.find(':')+1:]
+
+                queue_data["M"] = m
+                queue_data["A"] = a
+                queue_data["C"] = c
+
+                packets=[]
+
+                for i in range(m):
+                    packID = curr_line[:curr_line.find(',')].strip()
+                    curr_line = curr_line[curr_line.find(',')+1:].strip()
+                    packets.append(packID)
+
+                    if len(curr_line)>0  and curr_line[0]==';':
+                        if len(curr_line) > 1:
+                            curr_line = curr_line[1:]
+                        else:
+                            curr_line= ""
+
+                queue_data["Packets"] = packets
+                all_queues[name] = queue_data
+
+            if token in curr_cyc_data.keys():
+                for queue in all_queues:
+                    curr_cyc_data[token][queue] = all_queues[queue]
+            else:
+                curr_cyc_data[token]= all_queues
+            
+        cyc_data.append(curr_cyc_data)
+
+        os.system('cls')
+
     return cyc_data
+
+
+if __name__ == "__main__":
+    start_t = timeit.default_timer()
+    data = parse_data()
+    end_t = timeit.default_timer()
+    print("Time Required to load data: ", (end_t-start_t))
